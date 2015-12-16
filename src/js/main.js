0020b741d6baa32b0cfe9d1f5bd5f40ef0e6d8b7
Matt <mma@arillo.net>
2015-12-16 19:00:33 +0100

    Some improvements

--- src/js/main.js
+++ src/js/main.js
@@ -8,103 +8,150 @@ import plugin from 'plugin';
 // svg4everybody();
 
 $(() => {
-  $('body').on('click',function(){
-    hashy.go('three')
-  });
-
-  (function(){
-
-    const hashy = function(){
-
-      // This shoudl be fixed now jsut working when getting an selectByID
-
-      if(window.location.hash.length && window.location.hash != 'undefined'){
-
-        let cleanHash = window.location.hash.replace('#', '');
-        let elem = document.querySelector('[data-hash="' + cleanHash + '"]');
-
-        let element = document.getElementById(elem.id);
-        element.scrollIntoView()
-      }
+  $('*').on('click',function(){
+    hashy.go('two');
+  })
+  var hash = new hashy
+});
 
 
-      this.runtime = function(){};
+(function(){
+
+  const hashy = function(options){
+
+    if (! options) {
+      options = {};
+    };
+
+    this.elemClass = options.class || '.hashy-item';
+    this.dataAttr = options.data || 'data-hash'
+
+    // This shoudl be fixed now jsut working when getting an selectByID
+
+    if(window.location.hash.length && window.location.hash != 'undefined'){
+      let cleanHash = window.location.hash.replace('#', '');
+      let elem = document.querySelector('[' + this.dataAttr + '="' + cleanHash + '"]');
+      elem.scrollIntoView()
+    }
 
 
-      this.elemClass = '.hashy-item';
+    this.runtime = function(){};
 
-      // =============================
-      // Check when element is in window
-      // =============================
 
-      this.elems = document.querySelectorAll(this.elemClass);
 
-      hashy.checkElem = () =>{
+    // =============================
+    // Check when element is in window
+    // =============================
 
-        var scrollPos;
+    this.elems = document.querySelectorAll(this.elemClass);
 
-        scrollPos = () => {
-          var getPosition, scrollOffset;
-          scrollOffset = 0;
+    this.scrollOffset = 0;
 
-          getPosition = () => {
+    var selectedElem = null;
 
-            scrollOffset = window.pageYOffset || window.scrollTop;
+    hashy.checkElem = () =>{
 
-            if (scrollOffset === undefined) {
-              scrollOffset = 0;
-            };
-            var elemArray = Array.from(this.elems);
 
-            // Thisi s very important preventing the hashsetter to be triggered continously
-            var selectedElem;
+    var scrollPos = () => {
+        var getPosition, scrollOffset;
+        scrollOffset = 0;
 
-            elemArray.forEach( (elem) => {
-              var elemOffset = Math.abs(elem.offsetTop);
-              var elemHeight = elem.offsetHeight || elem.clientHeight
-              var height = window.innerHeight||document.documentElement.clientHeight;
-              if (scrollOffset + height > elemOffset && scrollOffset > elemOffset && scrollOffset < elemOffset + elemHeight ) {
-                var selectedElem = elem;
-              }
-              if (selectedElem) {
-                 hashy.setHash(selectedElem.getAttribute('data-hash'));
-              };
-            });
+        getPosition = () => {
 
-            hashy.runtime = requestAnimationFrame(getPosition);
+          this.scrollOffset = window.pageYOffset || window.scrollTop;
+
+          if (this.scrollOffset === undefined) {
+            this.scrollOffset = 0;
           };
 
 
-          return getPosition();
+          var elemArray = Array.from(this.elems);
 
+          elemArray.forEach( (elem) => {
+            var elemOffset = Math.abs(elem.offsetTop);
+            var elemHeight = elem.offsetHeight || elem.clientHeight;
+            var height = window.innerHeight||document.documentElement.clientHeight;
+            if (this.scrollOffset + height > elemOffset && this.scrollOffset > elemOffset && this.scrollOffset < elemOffset + elemHeight ) {
+              if (elem !== selectedElem ) {
+                selectedElem = elem;
+                hashy.setHash(selectedElem.getAttribute(this.dataAttr));
+              };
+            }
+          });
+
+          hashy.runtime = requestAnimationFrame(getPosition);
         };
 
-        scrollPos();
 
+        return getPosition();
+
+      };
+
+      scrollPos();
+
+    }
+
+    hashy.setHash = (hash) => {
+      if (window.location.hash !== hash) {
+        window.location.hash = hash;
+      } else {
+        return false;
       }
+    }
 
-      hashy.setHash = (hash) => {
-        console.log('called')
-        if (window.location.hash !== hash) {
-          window.location.hash = hash
-        } else {
-          return false;
+    hashy.go = (hash) => {
+      let elem = document.querySelector('[' + this.dataAttr + '="' + hash + '"]');
+      hashy.scrollTo(elem, hash);
+      hashy.setHash(hash);
+    }
+
+    hashy.scrollTo = (elem, hash) => {
+
+      cancelAnimationFrame(hashy.runtime);
+
+      let elemPos = Math.abs(elem.offsetTop);
+
+      if (elemPos > this.scrollOffset) {
+        var i = this.scrollOffset;
+        var y = elemPos;
+        var diff = y -i;
+        function scrollPlus() {
+          if(i < y){
+            i = i + diff/100;
+            document.body.scrollTop = i;
+          } else{
+           clearInterval(interval);
+           hashy.setHash(hash);
+           hashy.checkElem();
+          }
         }
-      }
-      // cancelAnimationFrame(hashy.runtime);
 
-      hashy.go = function(hash){
-        let elem = document.querySelector('[data-hash="' + hash + '"]');
-        let element = document.getElementById(elem.id);
-        element.scrollIntoView()
-        hashy.setHash(hash);
-      }
+        var interval = setInterval(scrollPlus, 1);
 
-      hashy.checkElem()
-    };
-    window.hashy = hashy;
-  }())
+      } else {
+        var i = this.scrollOffset;
+        var y = elemPos;
+        var diff = i - y;
+        function scrollMinus() {
+          if(i > y){
+            i = i - diff/100;
+            document.body.scrollTop = i;
+          } else{
+           clearInterval(interval);
+           hashy.setHash(hash);
+           hashy.checkElem();
+          }
+        }
 
-  var hash = new hashy
+        var interval = setInterval(scrollMinus, 1);
+      };
 
-});
+    }
+
+
+
+    hashy.checkElem()
+  };
+
+  window.hashy = hashy;
+}())
