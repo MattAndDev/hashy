2f41f45760b0d9071c52d005522896bc7f360850
Matt <mma@arillo.net>
2016-01-27 11:11:53 +0100

    Cleanup gh-pages branch

--- gulp/tasks/_default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var del = require('del');
-
-gulp.task('default', ['clean'], function(cb){
-  runSequence(['eslint'], ['sass', 'markup', 'browserify'],'libraryJs',  cb);
-});
