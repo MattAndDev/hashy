ac485fac7d3f82003713f15e2169567839b3b09e
Matt <mma@arillo.net>
2016-01-27 11:50:57 +0100

    More fixes, added offset option

--- gulp/tasks/libraryJs.js
+++ gulp/tasks/libraryJs.js
@@ -4,6 +4,7 @@ var gulp    = require('gulp');
 var config  = require('../config').library;
 var babel = require('gulp-babel');
 var size    = require('gulp-filesize');
+var handleErrors = require('../util/handleErrors');
 var uglify = require('gulp-uglify');
 
 gulp.task('libraryJs', ['browserify'], function() {
@@ -11,7 +12,9 @@ gulp.task('libraryJs', ['browserify'], function() {
     .pipe(babel({
       presets: ['es2015']
     }))
+    .on('error', handleErrors)
     .pipe(uglify())
+    .on('error', handleErrors)
     .pipe(gulp.dest(config.dest))
     .pipe(size());
 });
