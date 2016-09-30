bac983bd76b84c54758440033faae807d45f8ad2
Matteo Maronati <matteo@gentleman-studio.de>
2016-09-30 23:24:57 +0200

    Gulp to es6

---
+++ gulpfile.babel.js
@@ -0,0 +1,8 @@
+// ============================================
+// gulpfile.js
+// ============================================
+
+
+import requireDir from 'require-dir'
+
+requireDir('./gulp/tasks', { recurse: true })
